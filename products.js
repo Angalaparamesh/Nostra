5652c5c8d0a4a16c352739ad8e3d4e2c5d40d92d
Angalaparamesh <paramesh2000wari@gmail.com>
2024-08-31 22:40:21 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,89 @@
+const products = [
+    {
+        id:1,
+        name:"Floral Summer Shirt",
+        src:"product/f1.jpg",
+        desc:"",
+        price:220,
+        tags:["new","blue","summer"]
+    },
+
+    {
+        id:2,
+        name:"Summer Green",
+        src:"product/f2.jpg",
+        desc:"",
+        price:260,
+        tags:["new","green","beach"]
+    },
+
+    {
+        id:3,
+        name:"Party Floral Shirt",
+        src:"product/f3.jpg",
+        desc:"",
+        price:399,
+        tags:["old","red","party"]
+    },
+
+    {
+        id:4,
+        name:"Floral Summer Shirt",
+        src:"product/f4.jpg",
+        desc:"",
+        price:399,
+        tags:["old","white","beach"]
+    },
+    {
+        id:5,
+        name:"Beach Shirt",
+        src:"product/f5.jpg",
+        desc:"",
+        price:579,
+        tags:["old","white","beach"]
+    }
+,
+    {
+        id:6,
+        name:"Shirt Party Red",
+        src:"product/f6.jpg",
+        desc:"",
+        price:579,
+        tags:["old","red","party"]
+    },
+
+    {
+        id:7,
+        name:"Party Floral Shirt",
+        src:"product/f3.jpg",
+        desc:"",
+        price:399,
+        tags:["old","red","party"]
+    },
+    {
+        id:8,
+        name:"Shirt Party Red",
+        src:"product/f6.jpg",
+        desc:"",
+        price:579,
+        tags:["old","red","party"]
+    },
+    {
+        id:9,
+        name:"Floral Summer Shirt",
+        src:"product/f1.jpg",
+        desc:"",
+        price:220,
+        tags:["new","blue","summer"]
+    },
+
+
+]
+
+export {products}
+
+
+
+
+
+                        
